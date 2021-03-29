bacc8b1e82345279c45adca158c0550e24406ca6
vilarinux <davi.r.vilarinho@gmail.com>
2021-03-29 19:52:36 -0300

    Model Stock Prices Defined

---
+++ app/src/main/java/com/example/alertmestocks/Model/Prices.kt
@@ -0,0 +1,8 @@
+
+
+data class Prices(val tick,
+                  var last_price,
+                  var yesterday_price,
+                  var a_week_price,
+                  var last_month_price,
+                  var last_year_price) {// all this prices have to be parsed}
