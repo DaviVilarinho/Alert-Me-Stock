aac63e932009d4c58b803bb5ebb9b994fef69c99
vilarinux <davi.r.vilarinho@gmail.com>
2021-09-20 08:46:50 -0300

    i think i'm going to try connecting to my local db and check for bollinger bands :)

--- app/src/main/java/com/example/alertmestock/model/User.kt
+++
@@ -1,2 +0,0 @@
-data class User(val KEY:String,
-                val AUTOFETCH:Boolean)
--- app/src/main/java/com/example/alertmestock/service/FinProp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.alertmestock.service
-
-//import kotlinx.serialization.Serializable
-
-//@Serializable
-class FinProp(val symbol: String?,
-              val price:Float,
-              val volume:Double)
