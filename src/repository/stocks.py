bfdd762fadbbdb0a127fb91e901512041cfbe6a7
vilarinux <davi.r.vilarinho@gmail.com>
2021-09-21 10:59:36 -0300

    verifying key error

--- src/repository/stocks.py
+++ src/repository/stocks.py
@@ -28,13 +28,27 @@ class Stock:
         req.close()
 
     def isWarnable(self):
-        print("ok")
+        lastDaySync = self.findLastSyncDate()
+
+        try: 
+            refprice  = float(self.prices[lastDaySync.isoformat()]["4. close"])
+            bblow     = float(self.bollingers[lastDaySync.isoformat()]["Real Lower Band"])
+            bbhi      = float(self.bollingers[lastDaySync.isoformat()]["Real Upper Band"])
+
+            if refprice >= bbhi or refprice <= bblow:
+                return True
+            else:
+                return False
+        except KeyError: # if there's no date
+            return False # there's nothing to see
+        
+        
         
     def findLastSyncDate(self):
         today = datetime.date.today()
         for i in range(10): # try 10 different days
             try:
-                if self.bollingers[today.isoformat()] and self.prices[today.isoformat()]:
+                if self.bollingers[today.isoformat()] and self.prices[today.isoformat()]: # if today is the fetch day
                     return today
-            except KeyError:
+            except KeyError: # if not, subtract one day
                 today = today - datetime.timedelta(days=1)
