075ba1373f34c7c27771d702c3870c01ac05cc72
vilarinux <davi.r.vilarinho@gmail.com>
2021-09-21 11:04:22 -0300

    Logging in handler

--- src/handler/dbhandler.py
+++ src/handler/dbhandler.py
@@ -1,20 +1,27 @@
 import psycopg2
 import handler.credential as crd
+import logging
 
 GET_ALL_STOCKS = "SELECT id, tick, isUs FROM stocks"
 
 class DbHandler:
     def __init__(self):
-        self.con = psycopg2.connect("dbname='money' user='dv' password='" + crd.POSTGRES_CREDENTIAL + "'")
+        try:
+            self.con = psycopg2.connect("dbname='money' user='dv' password='" + crd.POSTGRES_CREDENTIAL + "'")
+            logging.info("Succesfully connected to postgresql")
+        except:
+            logging.error("Can't connect to postgresql")
         
     def getStocks(self):
         cur = self.con.cursor()
         cur.execute(GET_ALL_STOCKS)
         
         rows = cur.fetchall()
+        logging.info("Fetched Stocks: " + rows)
 
         cur.close()
         return rows
 
     def closeConnection(self):
         self.con.close()
+        logging.info("Closed connection to db")
