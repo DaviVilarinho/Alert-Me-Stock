33c0a2e2865c05b57204ff9476aa52f0914bfd67
vilarinux <davi.r.vilarinho@gmail.com>
2021-10-12 14:55:41 -0300

    rearranging vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,12 +8,6 @@ variable "ami_id" {
   }
 }
 
-variable "instance_name" {
-  description = "EC2 instance name"
-  type        = string
-  default       = "stocks_database"
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
